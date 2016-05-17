9bc0a9a15c4cf445c911aab8dc1b76eb6e4d81fc
chamlin <Chris.Hamlin@marklogic.com>
2016-05-16 22:31:43 -0400

    stacksum2 working across multiple pstack files

--- plutil/stacksum2.pl
+++ plutil/stacksum2.pl
@@ -5,21 +5,34 @@ $| = 1;
 use strict;
 use Data::Dumper;
 use Digest::MD5 qw(md5 md5_hex md5_base64);
+use List::Util qw(sum);
 
-my $filename = 'pstack.log';
 
-my $stats = do_file ($filename);
+my @filenames = @ARGV;
+
+my $stats = { dump => 0 };
+
+for my $filename (@filenames) {  do_file ($stats, $filename) }
 
 ready_stats ($stats);
+#print STDERR Dumper $stats;
 dump_stats ($stats);
 
-print STDERR Dumper $stats;
+
+
+########### subs
 
 sub dump_stats {
     my ($stats) = @_;
-    foreach my $sid (sort { $stats->{sig_sort_keys}{$b} cmp $stats->{sig_sort_keys}{$a} } (keys %{$stats->{sig_sort_keys}})) {
-        print "=========================================================\n";
-        print "@{$stats->{sig_counts}{$sid}}\n";
+    foreach my $sid (sort { $stats->{sig_sort_keys}{$b} <=> $stats->{sig_sort_keys}{$a} } (keys %{$stats->{sig_sort_keys}})) {
+        print "\n=========================================================\n";
+        foreach my $file_index (0 .. $#{$stats->{filenames}}) {
+            # avoid many rows of zeros
+            if (sum (@{$stats->{sig_counts}[$file_index]{$sid}}) > 0) {
+                print "@{$stats->{sig_counts}[$file_index]{$sid}}  - $stats->{filenames}[$file_index]\n";
+            }
+        }
+        print "@{$stats->{sig_count_totals}{$sid}}  - totals\n";
         print "$stats->{sig_text}{$sid}";
     }
 }
@@ -28,28 +41,37 @@ sub ready_stats {
     my ($stats) = @_;
     my $dumps = $stats->{dump};
     # uggh, fill in sparse counts, then create sort key (highest, to lowest, each slice)
-    foreach my $sid (keys %{$stats->{sig_counts}}) {
-        my $sig_counts = $stats->{sig_counts}{$sid};
-        foreach my $i (0 .. $dumps) {
-            unless (exists $sig_counts->[$i]) { $sig_counts->[$i] = 0 }
+    foreach my $file_counts (@{$stats->{sig_counts}}) {
+        foreach my $sid (keys %{$stats->{sig_text}}) {
+            my $sig_counts = $file_counts->{$sid};
+            foreach my $i (0 .. $dumps) {
+                unless (exists $file_counts->{$sid}[$i]) { $file_counts->{$sid}[$i] = 0 }
+                $stats->{sig_count_totals}{$sid}[$i] += ($sig_counts->[$i] ? $sig_counts->[$i] : 0);
+            }
+            $stats->{sig_sort_keys}{$sid} = join ('-', map { sprintf("%08d", $_) } @$sig_counts);
         }
-        $stats->{sig_sort_keys}{$sid} = join ('-', map { sprintf("%06d", $_) } @$sig_counts);
+    }
+    foreach my $sid (keys %{$stats->{sig_count_totals}}) {
+        #$stats->{sig_sort_keys}{$sid} = join ('-', map { sprintf("%08d", $_) } @{$stats->{sig_count_totals}{$sid}});
+        $stats->{sig_sort_keys}{$sid} = 
+             sum (@{$stats->{sig_count_totals}{$sid}})
+             *
+             10 ** sum ( map { $_ > 0 } @{$stats->{sig_count_totals}{$sid}} )
     }
 }
 
 sub do_file {
-    my ($filename) = @_;
+    my ($stats, $filename) = @_;
+    push @{$stats->{filenames}}, $filename;
     my $fh;
     open ($fh, '<', $filename) || die "Can't open $filename.\n";
-    my $stats = { 
-        dump => 0,
-    };
+    $stats->{dump} = 0;
     my $current = { lines => [] };
     while (my $line = <$fh>) {
         if ($line =~ /^#\d/) {
             push @{$current->{lines}}, $line;
         } elsif ($line =~ /^Thread .*Thread (0x[^\s]+) /) {
-            my $thread_id = $1;
+            my $thread_id = "$filename-$1";
             if (scalar (@{$current->{lines}})) {
                 file_thread ($stats, $current);
             }
@@ -71,9 +93,9 @@ sub file_thread {
     my $sig_text = join ('', map { my $t = $_; $t =~ s/#\d+\s+\S+ in //; $t } @{$thread->{lines}});
     $thread->{sig} = md5_hex ($sig_text);
     # increment sig count for this dump
-    ${$stats->{sig_counts}{$thread->{sig}}}[$stats->{dump}]++;
+    ${$stats->{sig_counts}[$#{$stats->{filenames}}]{$thread->{sig}}}[$stats->{dump}]++;
     # increment sig count for this thread
-    ${$stats->{thread_sigs}{$thread->{tid}}}{$thread->{sig}}++;
+    #${$stats->{thread_sigs}{$thread->{tid}}}{$thread->{sig}}++;
     # add (or replace . . .)
     $stats->{sig_text}{$thread->{sig}} = $thread->{text};
 }
